20320a19143b56a589be4e9b3a8a0b12e1b30708
nirajanmdr <31571079+nirajanmdr@users.noreply.github.com>
2017-10-04 02:57:53 +1100

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ firebase.initializeApp(config);
   var currentUser = null;
 
  //Handle Google Login
-/*    var uiConfig = {
+    var uiConfig = {
         signInSuccessUrl: 'dashboard.html',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
@@ -42,7 +42,7 @@ firebase.initializeApp(config);
       var ui = new firebaseui.auth.AuthUI(firebase.auth());
       // The start method will wait until the DOM is loaded.
       ui.start('#firebaseui-auth-container', uiConfig);
-*/
+
     //sign up form
 
     const txtEmail = document.getElementById('txtEmail');
